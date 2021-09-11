02764d472cb52b60321d4c8af70fa2355613f341
xpy123993 <xpy123993@gmail.com>
2021-09-11 15:26:24 -0700

    Add RPc webhook support

--- cmd/endpoint.go
+++ cmd/endpoint.go
@@ -2,13 +2,21 @@ package cmd
 
 import (
 	"context"
+	cryptorand "crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/xpy123993/yukino-net/libraries/endpointrpc/impl"
 	pb "github.com/xpy123993/yukino-net/libraries/endpointrpc/proto"
 	"github.com/xpy123993/yukino-net/libraries/util"
+	"golang.org/x/crypto/argon2"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -53,4 +61,47 @@ func StartEndPointService(ctx context.Context, ConfigFile, Channel string) error
 
 	log.Printf("Starting EndPoint service on channel `%s`", Channel)
 	return rpcServer.Serve(listener)
+}
+
+func GetHashToken(Token string) string {
+	data, err := base64.RawURLEncoding.DecodeString(Token)
+	if err != nil {
+		return "invalid"
+	}
+	return base64.RawURLEncoding.EncodeToString(argon2.IDKey(data, []byte("yapp/net/salt"), 1, 64*1024, 4, 64))
+}
+
+func StartEndPointWebhook(ctx context.Context, ConfigFile, LocalAddr, HashToken string) error {
+	limiter := rate.NewLimiter(3, 10)
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			http.Error(rw, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+		channel := strings.TrimPrefix(r.RequestURI, "/")
+		if len(channel) == 0 {
+			time.Sleep(time.Duration(1+2*rand.Float32()) * time.Second)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if GetHashToken(r.Header.Get("EndPoint-Service-Token")) != HashToken {
+			time.Sleep(time.Duration(1+2*rand.Float32()) * time.Second)
+			rw.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		InvokeEndPointShellProxyService(ctx, ConfigFile, channel, r.Header.Get("Command"), 5*time.Second)
+	})
+	return http.ListenAndServe(LocalAddr, nil)
+}
+
+func GenerateToken() error {
+	p := make([]byte, 64)
+	_, err := cryptorand.Read(p)
+	if err != nil {
+		return err
+	}
+	token := base64.RawURLEncoding.EncodeToString(p)
+	hashtoken := GetHashToken(token)
+	fmt.Printf("Token (EndPoint-Service-Token): %s\nWebHook HashToken: %s\n", token, hashtoken)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/spf13/cobra v1.2.1
 	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.40.0
@@ -19,4 +19,6 @@ require (
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 )
