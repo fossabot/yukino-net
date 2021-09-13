37f9bb82ea4bb8819a896715501e1f45920600f7
xpy123993 <xpy123993@gmail.com>
2021-09-12 20:05:37 -0700

    Remove upx to avoid anti-virus false postiive.

--- release-conf/release.sh
+++ release-conf/release.sh
@@ -9,15 +9,13 @@ VERSION_NUMBER=$1
 for ARCH in arm64 amd64; do
 echo "Building package for ${ARCH}"
 GOOS=linux GOARCH=$ARCH go build -ldflags "-s -w" -o build/$ARCH/yukino-net $SRC_FOLDER
-upx build/$ARCH/yukino-net
 go-bin-deb generate -a $ARCH --version "${VERSION_NUMBER?}" -w pkg-build/$ARCH/ -o $OUTPUT_FOLDER/yukino-net-$ARCH.deb
 done
 
 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o $OUTPUT_FOLDER/yukino-net.exe $SRC_FOLDER
-upx $OUTPUT_FOLDER/yukino-net.exe
+zip -9 $OUTPUT_FOLDER/yukino-net-windows-amd64.zip $OUTPUT_FOLDER/yukino-net.exe && rm $OUTPUT_FOLDER/yukino-net.exe
 
 GOOS=linux GOARCH=arm go build -ldflags "-s -w" -o build/armhf/yukino-net $SRC_FOLDER
-upx build/armhf/yukino-net
 go-bin-deb generate -a armhf --version "${VERSION_NUMBER?}" -w pkg-build/armhf/ -o $OUTPUT_FOLDER/yukino-net-armhf.deb
 
 rm -rf pkg-build
