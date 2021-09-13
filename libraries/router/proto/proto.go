db69579a20c90a147ab670e239c9d1f9105736e2
xpy123993 <xpy123993@gmail.com>
2021-09-12 23:30:17 -0700

    Fix some lint errors.

--- libraries/router/proto/proto.go
+++ libraries/router/proto/proto.go
@@ -1,15 +1,15 @@
 package proto
 
 const (
-	// Indicates the frame contains a dial request. Controlled by Invoke ACL.
+	// Dial indicates the frame contains a dial request. Controlled by Invoke ACL.
 	Dial = byte(iota)
-	// Indicates the frame contains a listen request. Controlled by Listen ACL.
+	// Listen indicates the frame contains a listen request. Controlled by Listen ACL.
 	Listen = byte(iota)
-	// Indicates the frame contains a bridge request. Controlled by Listen ACL.
+	// Bridge indicates the frame contains a bridge request. Controlled by Listen ACL.
 	// This request should contain a connection ID for the server to set a bridge betwen a dial request.
 	Bridge = byte(iota)
-	// Indicates the frame is just for a ping. No ACL action specific control.
+	// Nop indicates the frame is just for a ping. No ACL action specific control.
 	Nop = byte(iota)
-	// Indicates the connection is closed. No ACL action specific control.
+	// Close indicates the connection is closed. No ACL action specific control.
 	Close = byte(iota)
 )
